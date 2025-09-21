0064ce253df05e8b148a602e911f7b404c327db3
Viviana <vivi.iaco@gmail.com>
2025-09-20 22:33:33 -0300

    Ajuste porta

--- app/app.py
+++ app/app.py
@@ -220,4 +220,4 @@ def avaliar_entrevista():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
